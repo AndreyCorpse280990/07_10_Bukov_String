384c76b9e52ad83f3217e7e88f068453e8adc3e6
AndreyCorpse28090 <280990corpse@gmail.com>
2023-10-16 21:40:47 +0300

    1

--- 07_10_Bukov_String/07_10_Bukov_String.cpp
+++ 07_10_Bukov_String/07_10_Bukov_String.cpp
@@ -1,20 +1,73 @@
-﻿// 07_10_Bukov_String.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>
 
-int main()
+class String 
 {
-    std::cout << "Hello World!\n";
+private:
+    char* data;
+    size_t length;
+
+public:
+    // Конструктор по умолчанию
+    String();
+
+    // Конструктор с инициализацией C-строкой
+    String(const char* cstr);
+
+    // Конструктор перемещения
+    String(String&& other);
+
+    // Деструктор
+    ~String();
+
+    // Метод для получения указателя на данные
+    const char* c_str() const;
+};
+
+// Реализация конструктора по умолчанию
+String::String() : data(nullptr), length(0) {}
+
+// Реализация конструктора с инициализацией C-строкой
+String::String(const char* cstr) : data(nullptr), length(0) 
+{
+    length = strlen(cstr);
+    data = new char[length + 1];
+    strncpy(data, cstr, length);
+    data[length] = '\0';
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+// Реализация конструктора перемещения
+String::String(String&& other) : data(nullptr), length(0) 
+{
+    // Перемещаю  из other
+    data = other.data;
+    length = other.length;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    // Обнуляю other
+    other.data = nullptr;
+    other.length = 0;
+}
+
+// Реализация деструктора
+String::~String() {
+    delete[] data;
+}
+
+// Реализация метода c_str()
+const char* String::c_str() const 
+{
+    return data;
+}
+
+int main() 
+{
+    const char* cstr = "Hello, World!";
+    String str(cstr);
+
+    String str2(std::move(str)); 
+
+    std::cout << "str2: " << str2.c_str() << std::endl;
+
+    return 0;
+}
